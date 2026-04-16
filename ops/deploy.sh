369b85b6d8c78e3d70ed98a174336fd068a088c7
Sanqui <me@sanqui.net>
2026-04-16 18:43:47 +0200

    Add deploy script

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -3,15 +3,17 @@ set -e
 echo 'Launching deploy script for rhweb2'
 
 SERVER="rhbox.hernihistorie.cz"
+DEPLOY_BRANCH="master"
 
 ssh -t $SERVER "
     echo 'Please enter password for $USER@$SERVER' &&
-    sudo -u deploy -i bash -c '
-        cd /var/www/rhweb2/ &&
+    sudo -u flask -i bash -c '
+        cd /var/www/rhweb2_/rhweb2 &&
+        echo Updating code... &&
         git fetch origin $DEPLOY_BRANCH &&
-        git reset --hard origin/$DEPLOY_BRANCH &&
-        docker build -t haweb .
+        git reset --hard origin/$DEPLOY_BRANCH
     ' &&
-    sudo systemctl restart haweb.service
+    echo Restarting rhweb2 service...
+    sudo systemctl restart rhweb2.service &&
     echo Done!
 "
