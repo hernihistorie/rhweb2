e1fca44cf68fff28f1b440957a9724d4dacb0663
Sanqui <me@sanqui.net>
2026-04-16 18:35:16 +0200

    Stop using dropped psycopg2.tz

--- rhforum.py
+++ rhforum.py
@@ -25,13 +25,7 @@ from wtforms import Form, BooleanField, StringField as TextField, TextAreaField,
 import requests
 
 def now():
-    if app.config['DB'].startswith('postgresql+psycopg2'):
-        # https://stackoverflow.com/questions/796008/cant-subtract-offset-naive-and-offset-aware-datetimes/17752647#17752647
-        import psycopg2
-        return datetime.utcnow().replace(
-            tzinfo=psycopg2.tz.FixedOffsetTimezone(offset=0, name=None))
-    else:
-        return datetime.utcnow()
+    return datetime.now()
 
 dtnow = now
 
